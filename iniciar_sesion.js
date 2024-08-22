e605da137d1a2b962d32db206309643b22e77022
Jiun Ming Hsu <hsujm219@gmail.com>
2024-08-22 00:36:23 -0300

    initial commit

---
+++ iniciar_sesion.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loginBtn = document.getElementById('login-btn');
+  const signUpBtn = document.getElementById('signup-btn');
+
+  loginBtn.addEventListener('click', () => {
+    const formContainer = document.getElementById('formulario');
+
+    formContainer.innerHTML += `        
+    `;
+  });
+
+  signUpBtn.addEventListener('click', () => {
+    output.innerHTML = 'Hello World!';
+  });
+});
